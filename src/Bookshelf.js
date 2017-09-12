ee70a991bbe0de309d6cd6e3d576f64e7449344a
Murillo de Morais <murillo.morais@vianuvem.com.br>
2017-09-12 16:19:43 -0300

    feat: added Pages and Bookshelf components

---
+++ src/Bookshelf.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class Bookshelf extends Component{
+    render() {
+        let { name, status, books } = this.props;
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{name}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {books.map(book => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                        <div className="book-shelf-changer">
+                                            <select value={status} readOnly>
+                                                <option value="none" disabled>Move to...</option>
+                                                <option value="currentlyReading">Currently Reading</option>
+                                                <option value="wantToRead">Want to Read</option>
+                                                <option value="read">Read</option>
+                                                <option value="none">None</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Bookshelf
---
+++ src/ListBooks.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Bookshelf from './Bookshelf'
+
+class ListBooks extends Component{
+    render() {
+        const books = [
+            {
+                id: 1,
+                title: '1776',
+                authors: 'David McCullough',
+                image: 'http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api'
+            },
+            {
+                id: 2,
+                title: 'Harry Potter and the Sorcerer\'s Stone',
+                authors: 'J.K. Rowling',
+                image: 'http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api'
+            }
+        ];
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        <Bookshelf name="Currently Reading" books={books} status="currentlyReading"/>
+                        <Bookshelf name="Want to Read" books={books} status="wantToRead"/>
+                        <Bookshelf name="Read" books={books} status="read"/>
+                    </div>
+                </div>
+                <div className="open-search">
+                    <Link to="/search">Add a book</Link>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListBooks
